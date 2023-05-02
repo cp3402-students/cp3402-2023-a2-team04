739719471ade94bd027269a50623e66d6dd1b3c1
Matt Marsh <matthew.marsh@my.jcu.edu.au>
2023-05-02 20:12:02 +1000

    Remove bootstrap directory and usages and update deployment markdown

--- functions.php
+++ functions.php
@@ -143,10 +143,6 @@ function heartland_hits_scripts() {
 
 	wp_enqueue_script( 'heartland-hits-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
-    // Adding Bootstrap to the Theme
-    wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css');
-    wp_enqueue_script( 'bootstrap-js' , get_template_directory_uri() . '/bootstrap/js/bootstrap.bundle.min.js', array('jquery') );
-    
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
