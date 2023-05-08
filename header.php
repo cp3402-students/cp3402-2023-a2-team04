91dbda43cef4eadbf088c6e0b307347b2eeb7dba
danniellejones <dannielle.jones1@my.jcu.edu.au>
2023-05-09 03:18:31 +1000

    Add custom logo setup

--- header.php
+++ header.php
@@ -35,25 +35,27 @@
 
     <header id="masthead" class="site-header">
         <div class="site-branding">
-            <?php
-            the_custom_logo();
-            if (is_front_page() && is_home()) :
-                ?>
-                <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
-                                          rel="home"><?php bloginfo('name'); ?></a></h1>
-            <?php
-            else :
-                ?>
-                <p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
-                                         rel="home"><?php bloginfo('name'); ?></a></p>
-            <?php
-            endif;
-            $heartland_hits_description = get_bloginfo('description', 'display');
-            if ($heartland_hits_description || is_customize_preview()) :
-                ?>
-                <p class="site-description"><?php echo $heartland_hits_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-                    ?></p>
-            <?php endif; ?>
+            <?php the_custom_logo(); ?>
+            <div class="site-branding-text">
+                <?php
+                if (is_front_page() && is_home()) :
+                    ?>
+                    <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                              rel="home"><?php bloginfo('name'); ?></a></h1>
+                <?php
+                else :
+                    ?>
+                    <p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                             rel="home"><?php bloginfo('name'); ?></a></p>
+                <?php
+                endif;
+                $heartland_hits_description = get_bloginfo('description', 'display');
+                if ($heartland_hits_description || is_customize_preview()) :
+                    ?>
+                    <p class="site-description"><?php echo $heartland_hits_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+                        ?></p>
+                <?php endif; ?>
+            </div>
         </div><!-- .site-branding -->
 
         <nav id="site-navigation" class="main-navigation">
