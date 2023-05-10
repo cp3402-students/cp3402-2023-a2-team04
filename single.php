5768c4364da772f47b65a4177bef2897ff00004d
danniellejones <dannielle.jones1@my.jcu.edu.au>
2023-05-10 20:13:59 +1000

    Fixes to footer and single-newsletter

--- single.php
+++ single.php
@@ -25,7 +25,7 @@ get_header();
 				)
 			);
 
-			// If comments are open or we have at least one comment, load up the comment template.
+			// If comments are open, or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
 			endif;
