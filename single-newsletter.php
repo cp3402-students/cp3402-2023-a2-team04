3ad91a3fe4db200b915d1c55230dab6669cde5b5
danniellejones <dannielle.jones1@my.jcu.edu.au>
2023-05-10 17:36:15 +1000

    Add single event page template

--- single-newsletter.php
+++ single-newsletter.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all single posts
+ * The template for displaying all single posts with the category newsletter
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
